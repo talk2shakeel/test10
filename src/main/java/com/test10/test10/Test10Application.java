36ee6b6242a97ef81db5723a8e8faf247ad6b5cc
shakeel <self.shakeelahmed@gmail.com>
2025-03-13 11:54:23 +0530

    First Commit

---
+++ src/main/java/com/test10/test10/Test10Application.java
@@ -0,0 +1,13 @@
+package com.test10.test10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test10Application.class, args);
+	}
+
+}
